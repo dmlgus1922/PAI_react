5100817f4028b8aba23484236e8d952801af19cf
김의현 <dmlgus1922@naver.com>
2024-06-25 16:06:42 +0900

    Update ChatCompo.tsx

--- chat-next/components/ChatCompo.tsx
+++ chat-next/components/ChatCompo.tsx
@@ -13,7 +13,7 @@ interface Message {
 }
 
 let socket: WebSocket;
-
+// let hcxContent: string = '';
 const gptWelcomeText = '안녕하세요. 무엇을 도와드릴까요?';
 
 const ChatCompo = () => {
@@ -21,46 +21,64 @@ const ChatCompo = () => {
         { 'role': 'assistant', 'content': gptWelcomeText },
     ]);
     const [input, setInput] = useState<string>('');
+    const [isWsConnected, setIsWsConnected] = useState<boolean>(false);
     const [hcxContent, setHcxContent] = useState<string>('');
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const messagesEndRef = useRef<HTMLDivElement>(null);
     const inputRef = useRef<HTMLTextAreaElement>(null);
 
+    const handleSetMessages = () => {
+        console.log('handleSetMessages 호출')
+        setMessages([
+            ...messages,
+            { role: 'user', content: input },
+            { 'role': 'assistant', 'content': hcxContent }
+        ]);
+    };
+
     useEffect(() => {
         socket = new WebSocket('ws://localhost:9999/hcx');
         socket.onopen = () => {
             console.log('Connected to server');
+            // setIsWsConnected(true);
         };
-
+        // onmessage 때마다 messages상태가 바뀌어야 함
+        // 첫 onmessage 이후 ㄹ
         socket.onmessage = (event) => {
             const msg = event.data;
+            console.log('received_msg:', msg, '  시각:', Date.now(), '\ninput:', input);
             if (msg !== 'hcx finished') {
-                setHcxContent(hcxContent + msg);
-                console.log(hcxContent);
-                setIsLoading(false);
-
+                // setHcxContent(msg);
+                const msgData = JSON.parse(msg)
+                setMessages(msgData.msgs);
+                // handleSetMessages();
             } else {
                 // 파이썬 서버에서 메시지를 모두 보냄을 확인했을 때 load false임을 전송
                 setIsLoading(false);
             }
+
         }
 
         socket.onclose = () => {
             console.log('disconnected from server');
-        }
+            setIsWsConnected(!isWsConnected);
+            setInput('');
+            // console.log(messages);
         
+        }
+
+    }, [isWsConnected, hcxContent]);
+
+    useEffect(() => {
         scrollToBottom();
         inputRef.current.focus();
-        // return () => {
-        //     socket.close();
-        // }
+        
     }, [messages]);
 
     const sendMessage = (jsonData: string) => {
         // 서버에 데이터(대화 내역)를 전송할 때에는 
         // 클라이언트에서 모두 작업이 끝난 후여야 함
         socket.send(jsonData);
-        
     }
 
     const getHCXResponse = () => {
